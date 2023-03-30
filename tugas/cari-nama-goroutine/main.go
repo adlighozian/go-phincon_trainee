47cb36e9237aa757fbbcb0b0f258bde0df79c77c
adlighozian <adlighozian1@gmail.com>
2023-03-31 05:39:36 +0700

    5:39

--- tugas/cari-nama-goroutine/main.go
+++ tugas/cari-nama-goroutine/main.go
@@ -10,36 +10,60 @@ import (
 	"time"
 )
 
+var wg = new(sync.WaitGroup)
+
 func main() {
 	helper.CallClear()
-	var wg sync.WaitGroup
-	const timeoutDuration = 3 * time.Second
+
+	const timeoutDuration = 5 * time.Hour
 
 	ctx, cancel := context.WithTimeout(context.Background(), timeoutDuration)
-
 	defer cancel()
 
-	wg.Add(1)
-	go func() {
-		readNamesWorker(ctx, &wg, model.Names)
-		wg.Done()
-	}()
+	// wg.Add(2)
 
-	wg.Wait()
+	getNames := getName(ctx, model.Names)
+	filterName(getNames, ctx)
+	var input string
+	fmt.Scanln(&input)
+	// wg.Wait()
 }
 
-func readNamesWorker(ctx context.Context, wg *sync.WaitGroup, names []string) {
-	numberOfWorkers := 10
+func getName(ctx context.Context, names []string) <-chan string {
+	chanOut := make(chan string, 5)
+
+	go func() {
+		select {
+		case <-ctx.Done():
+			break
+		default:
+			for _, v := range names {
+				chanOut <- v
+				// time.Sleep(1 * time.Second)
+			}
+		}
+		defer close(chanOut)
+	}()
+
+	return chanOut
+}
+
+func filterName(chanIn <-chan string, ctx context.Context) {
+	numberOfWorkers := 5
 	wg.Add(numberOfWorkers)
+
 	go func() {
 		fmt.Println("start check name with worker", numberOfWorkers)
 		for workerIndex := 1; workerIndex <= numberOfWorkers; workerIndex++ {
 			var total int
+			// time.Sleep(1 * time.Second)
 			go func(workerIndex int) {
 				defer wg.Done()
 				start := time.Now()
 			L:
-				for _, name := range names {
+				for name := range chanIn {
+					// fmt.Println(name)
+
 					select {
 					case <-ctx.Done():
 						break L
@@ -49,10 +73,8 @@ func readNamesWorker(ctx context.Context, wg *sync.WaitGroup, names []string) {
 						}
 					}
 				}
-				
 				duration := time.Since(start)
 				fmt.Println("worker", workerIndex, "found", total, "of Wina. Done in", duration.Seconds(), "seconds")
-
 			}(workerIndex)
 		}
 
